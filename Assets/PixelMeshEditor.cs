be4b4edbef91be42cc2e5abd7c8c9bd934f934c7
Finlay Christ <RandomiaGaming@gmail.com>
2023-11-29 21:35:33 -0800

    Backup Commit

--- Assets/PixelMeshEditor.cs
+++ Assets/PixelMeshEditor.cs
@@ -1,18 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PixelMeshEditor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public EditorPixelMeshFilter EPMF;
     void Update()
     {
-        
+
     }
 }
